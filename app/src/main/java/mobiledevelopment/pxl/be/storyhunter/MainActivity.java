41df931d508d82508494c6cfa64862877d7f0e46
JonasKempeneers <jonaskempeneers193@gmail.com>
2018-09-25 16:27:32 +0200

    Added button that changes color on click

--- app/src/main/java/mobiledevelopment/pxl/be/storyhunter/MainActivity.java
+++ app/src/main/java/mobiledevelopment/pxl/be/storyhunter/MainActivity.java
@@ -1,13 +1,39 @@
 package mobiledevelopment.pxl.be.storyhunter;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button colorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         colorButton = findViewById(R.id.button);
+         colorButton.setBackgroundColor(Color.WHITE);
+
+         colorButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 ColorDrawable buttonColor = (ColorDrawable) colorButton.getBackground();
+
+                 if(buttonColor.getColor() == Color.BLUE){
+                     colorButton.setBackgroundColor(Color.WHITE);
+                 } else {
+                     colorButton.setBackgroundColor(Color.BLUE);
+                 }
+
+             }
+         });
+    }
+
+    public void changeColor(){
+
     }
 }
