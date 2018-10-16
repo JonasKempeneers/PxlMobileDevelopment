f84fbe3c0b321c79a4a1814ebf18c0dca957d6aa
JonasKempeneers <jonaskempeneers193@gmail.com>
2018-10-16 13:51:17 +0200

    Changed layout of main screen
    Main activity now has a layout so the user can login.
    Changed the colors in de resources.

--- app/src/main/java/mobiledevelopment/pxl/be/storyhunter/MainActivity.java
+++ app/src/main/java/mobiledevelopment/pxl/be/storyhunter/MainActivity.java
@@ -7,16 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button colorButton;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+
 
     public void goToMenu(View view) {
         Intent intent = new Intent(MainActivity.this, BookListActivity.class);
@@ -36,16 +42,5 @@ public class MainActivity extends AppCompatActivity {
     public void navigateToQRGenerator(View view){
         Intent intent = new Intent(this, QRCodeCreateActivity.class);
         startActivity(intent);
-    }
-
-    public void changeColor(View view) {
-        colorButton = findViewById(R.id.button);
-        ColorDrawable buttonColor = (ColorDrawable) colorButton.getBackground();
-
-        if(buttonColor.getColor() == Color.BLUE){
-            colorButton.setBackgroundColor(Color.WHITE);
-        } else {
-            colorButton.setBackgroundColor(Color.BLUE);
-        }
     }
 }
