08abc5bcb1b21fddff42502685cbe4d3b82c7606
Unknown <jonathan.godeyne@student.pxl.be>
2018-11-12 09:09:34 +0100

    Fixed merge error on SimpleReyclerViewAdapter

--- app/src/main/java/mobiledevelopment/pxl/be/storyhunter/adapters/SimpleItemRecyclerViewAdapter.java
+++ app/src/main/java/mobiledevelopment/pxl/be/storyhunter/adapters/SimpleItemRecyclerViewAdapter.java
@@ -20,74 +20,71 @@ import mobiledevelopment.pxl.be.storyhunter.entities.Book;
 public class SimpleItemRecyclerViewAdapter
         extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-        private final BookListActivity mParentActivity;
-        private final List<Book> mValues;
-        private final boolean mTwoPane;
-       private final boolean mIsPlacedBook;
-        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Book item = (Book) view.getTag();
-                if (mTwoPane) {
-                    Bundle arguments = new Bundle();
-                    arguments.putString(BookDetailFragment.BOOK_ID, Integer.toString(item.getId()));
-                    BookDetailFragment fragment = new BookDetailFragment();
-                    fragment.setArguments(arguments);
-                    mParentActivity.getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.book_detail_container, fragment)
-                            .commit();
-                } else {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, BookDetailActivity.class);
-                    intent.putExtra(BookDetailFragment.BOOK_ID, Integer.toString(item.getId()));
-                    intent.putExtra("isPlacedBook", mIsPlacedBook);
-                    context.startActivity(intent);
-                }
+    private final BookListActivity mParentActivity;
+    private final List<Book> mValues;
+    private final boolean mTwoPane;
+    private final boolean mIsPlacedBook;
+    private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Book item = (Book) view.getTag();
+            if (mTwoPane) {
+                Bundle arguments = new Bundle();
+                arguments.putString(BookDetailFragment.BOOK_ID, Integer.toString(item.getId()));
+                BookDetailFragment fragment = new BookDetailFragment();
+                fragment.setArguments(arguments);
+                mParentActivity.getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.book_detail_container, fragment)
+                        .commit();
+            } else {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, BookDetailActivity.class);
+                intent.putExtra(BookDetailFragment.BOOK_ID, Integer.toString(item.getId()));
+                intent.putExtra("isPlacedBook", mIsPlacedBook);
+                context.startActivity(intent);
             }
-        };
-
-        public SimpleItemRecyclerViewAdapter(BookListActivity parent,
-                                             List<Book> items,
-                                             boolean twoPane,
-                                             boolean isPlacedBook) {
-            mValues = items;
-            mParentActivity = parent;
-            mTwoPane = twoPane;
-            mIsPlacedBook = isPlacedBook;
         }
     };
 
     public SimpleItemRecyclerViewAdapter(BookListActivity parent,
                                          List<Book> items,
-                                         boolean twoPane) {
+                                         boolean twoPane,
+                                         boolean isPlacedBook) {
         mValues = items;
         mParentActivity = parent;
         mTwoPane = twoPane;
+        mIsPlacedBook = isPlacedBook;
     }
 
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-           holder.mIdView.setText(Integer.toString(mValues.get(position).getId()));
-            holder.mContentView.setText(mValues.get(position).getTitle());
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.book_list_content, parent, false);
+        return new ViewHolder(view);
+    }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        // holder.mIdView.setText(Integer.toString(mValues.get(position).getId()));
+        holder.mIdView.setText(Integer.toString(mValues.get(position).getId()));
         holder.mContentView.setText(mValues.get(position).getTitle());
 
         holder.itemView.setTag(mValues.get(position));
         holder.itemView.setOnClickListener(mOnClickListener);
     }
 
-        class ViewHolder extends RecyclerView.ViewHolder {
-           final TextView mIdView;
-            final TextView mContentView;
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
 
-            ViewHolder(View view) {
-                super(view);
-                mIdView = (TextView) view.findViewById(R.id.id_text);
-                mContentView = (TextView) view.findViewById(R.id.content);
-            }
+    class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView mIdView;
+        final TextView mContentView;
+
+        ViewHolder(View view) {
+            super(view);
+            mIdView = (TextView) view.findViewById(R.id.id_text);
+            mContentView = (TextView) view.findViewById(R.id.content);
         }
     }
 }
